d8b7c6ba5eefea7ae2e301ef2fe9223ea874a981
Seif BELHADJALI <seifeddine.belhadgali.ext@abeille-assurances.fr>
2024-02-20 08:57:14 +0100

    delete src main

--- src/main/java/fr/aba/prevoyance/domain/constante/produit/ConstantesProduits.java
+++
@@ -1,9 +0,0 @@
-package fr.aba.prevoyance.domain.constante.produit;
-
-public class ConstantesProduits {
-	public final static String LIBELLE_PRODUIT_SP1_V = "SP1 V";
-	public final static String LIBELLE_PRODUIT_SE4_V = "SE4 V";
-	public final static String LIBELLE_PRODUIT_SE4MV = "SE4MV";
-	public final static String LIBELLE_PRODUIT_SE4LV = "SE4LV";
-	public final static String LIBELLE_PRODUIT_SE4GV = "SE4GV";
-}
--- src/main/java/fr/aba/prevoyance/domain/constante/risque/ConstantesRisques.java
+++
@@ -1,7 +0,0 @@
-package fr.aba.prevoyance.domain.constante.risque;
-
-public class ConstantesRisques {
-	public final static String AUGMENTATION = "AUGMENTATION";
-	public final static String SANS_IMPACT = "SANS_IMPACT";
-	public final static String DIMINUTION = "DIMINUTION";
-}
